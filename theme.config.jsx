b5b17b2c235c951678e828acf3ff68549eb23327
Amit Jakubowicz <amiiit@users.noreply.github.com>
2023-09-13 16:25:44 +0200

    Adapt logo to theme

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,13 @@
+import { useTheme } from 'next-themes'
+
 export default {
-	logo: <span>Ultimate API Documentation</span>,
-	project: {
-		link: 'https://github.com/ultimateai'
-	}
-	// ... other theme options
+    logo: (props) => {
+        const { resolvedTheme } = useTheme()
+        const src = resolvedTheme === 'dark' ? '/ultimate-white.png' : '/ultimate-black.png';
+        return <img width={150} height={33} src={src}/>
+    },
+    project: {
+        link: 'https://github.com/ultimateai'
+    }
+    // ... other theme options
 }
