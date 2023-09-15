30ae7843d7345d641390045d7a2cce7dad19f1e9
Amit Jakubowicz <amiiit@users.noreply.github.com>
2023-09-15 13:13:19 +0200

    Just ignore what you cannot fix

--- components/APIDocs/ElementsAPIDocs.tsx
+++ components/APIDocs/ElementsAPIDocs.tsx
@@ -1,9 +1,11 @@
+// @ts-ignore
 import {API} from '@stoplight/elements';
 import '@stoplight/elements/styles.min.css';
 
 const ElementsAPIDocs = () => {
 	return <API
 		apiDescriptionUrl="/swagger.yaml"
+
 		router="hash"
 	/>
 }
